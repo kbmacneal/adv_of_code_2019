0f1a76525b474f97382d58227f77eb37b7e21981
bowmorek <kenneth.b.macneal@gmail.com>
2019-12-12 14:38:19 -0600

    reformat and optimization

--- days/12.cs
+++ days/12.cs
@@ -5,34 +5,42 @@ using System.Threading.Tasks;
 using adv_of_code_2019.Classes;
 using VectorAndPoint.ValTypes;
 
-namespace adv_of_code_2019 {
-    public class Day12 {
-        private class moon : IEquatable<moon> {
+namespace adv_of_code_2019
+{
+    public class Day12
+    {
+        private class moon : IEquatable<moon>
+        {
 
             public Point3D Position { get; set; }
             public Vector3D Velocity { get; set; } = new Vector3D (0, 0, 0);
             public int p_energy { get; set; }
             public int k_energy { get; set; }
 
-            public moon (int X, int Y, int Z) {
+            public moon (int X, int Y, int Z)
+            {
                 this.Position = new Point3D (X, Y, Z);
             }
 
-            public bool Equals (moon m) {
+            public bool Equals (moon m)
+            {
                 return this.Position == m.Position && this.Velocity == m.Velocity;
             }
         }
 
-        public static async Task Run () {
+        public static async Task Run ()
+        {
             List<moon> moons = RefreshMoons ();
 
             var stepcount = 1000;
 
-            for (int i = 0; i < stepcount; i++) {
+            for (int i = 0; i < stepcount; i++)
+            {
                 StepVelo (ref moons);
             }
 
-            foreach (var moon in moons) {
+            foreach (var moon in moons)
+            {
                 moon.p_energy = ((int) moon.Position.X).Abs () + ((int) moon.Position.Y).Abs () + ((int) moon.Position.Z).Abs ();
 
                 moon.k_energy = ((int) moon.Velocity.X).Abs () + ((int) moon.Velocity.Y).Abs () + ((int) moon.Velocity.Z).Abs ();
@@ -44,35 +52,44 @@ namespace adv_of_code_2019 {
 
             moons = RefreshMoons ();
             var orig_moons = RefreshMoons ();
-            var intervals = new int[3];
+            var intervals = new int [3];
             var completed = false;
             var c = 0;
 
-            while (!completed) {
+            while (!completed)
+            {
 
                 StepVelo (ref moons);
                 c++;
 
-                for (var axis = 0; axis < 3; axis++) {
+                for (var axis = 0; axis < 3; axis++)
+                {
                     // go through X => Y =>Z
                     // find the interval that each axis processes through
                     // compare to 0 to not recalculate the period of an axis twice
-                    if (intervals[axis] == 0 &&
-                        AreAxisStatesEqual (orig_moons.ToArray (), moons.ToArray (), (Axis) axis)) {
-                        intervals[axis] = c;
-                        //complete when all the axes have a period associated with them
-                        if (intervals.All (x => x > 0)) {
-                            completed = true;
-                            break;
+                    if (intervals [axis] == 0)
+                    {
+                        if (AreAxisStatesEqual (orig_moons, moons, (Axis) axis))
+                        {
+                            intervals [axis] = c;
+                            //complete when all the axes have a period associated with them
+                            if (intervals.All (x => x > 0))
+                            {
+                                completed = true;
+                                break;
+                            }
                         }
                     }
+
                 }
             }
-            Console.WriteLine ("Part 2: " + intervals.GetLCM().ToString());
+            Console.WriteLine ("Part 2: " + intervals.GetLCM ().ToString ());
         }
 
-        private static List<moon> RefreshMoons () {
-            return new List<moon> () {
+        private static List<moon> RefreshMoons ()
+        {
+            return new List<moon> ()
+            {
                 new moon (17, -12, 13),
                     new moon (2, 1, 1),
                     new moon (-1, -17, 7),
@@ -80,12 +97,15 @@ namespace adv_of_code_2019 {
             };
         }
 
-        private static void StepVelo (ref List<moon> moons) {
+        private static void StepVelo (ref List<moon> moons)
+        {
             var length = moons.Count;
-            for (var indexA = 0; indexA < length; indexA++) {
-                var moonA = moons[indexA];
-                for (var indexB = indexA + 1; indexB < length; indexB++) {
-                    var moonB = moons[indexB];
+            for (var indexA = 0; indexA < length; indexA++)
+            {
+                var moonA = moons [indexA];
+                for (var indexB = indexA + 1; indexB < length; indexB++)
+                {
+                    var moonB = moons [indexB];
                     var diffX = moonA.Position.X.CompareTo (moonB.Position.X);
                     var diffY = moonA.Position.Y.CompareTo (moonB.Position.Y);
                     var diffZ = moonA.Position.Z.CompareTo (moonB.Position.Z);
@@ -98,22 +118,25 @@ namespace adv_of_code_2019 {
             }
         }
 
-        private static bool AreAxisStatesEqual (moon[] original, moon[] current, Axis axis) {
-            var length = original.Length;
-            for (var i = 0; i < length; i++) {
+        private static bool AreAxisStatesEqual (List<moon> original, List<moon> current, Axis axis)
+        {
+            var length = original.Count;
+            for (var i = 0; i < length; i++)
+            {
                 bool rtn = false;
-                switch (axis) {
-                    case Axis.X:
-                        rtn = original[i].Position.X != current[i].Position.X || original[i].Velocity.X != current[i].Velocity.X;
-                        break;
-                    case Axis.Y:
-                        rtn = original[i].Position.Y != current[i].Position.Y || original[i].Velocity.Y != current[i].Velocity.Y;
-                        break;
-                    case Axis.Z:
-                        rtn = original[i].Position.Z != current[i].Position.Z || original[i].Velocity.Z != current[i].Velocity.Z;
-                        break;
-                    default:
-                        throw new InvalidOperationException ();
+                switch (axis)
+                {
+                case Axis.X:
+                    rtn = original [i].Position.X != current [i].Position.X || original [i].Velocity.X != current [i].Velocity.X;
+                    break;
+                case Axis.Y:
+                    rtn = original [i].Position.Y != current [i].Position.Y || original [i].Velocity.Y != current [i].Velocity.Y;
+                    break;
+                case Axis.Z:
+                    rtn = original [i].Position.Z != current [i].Position.Z || original [i].Velocity.Z != current [i].Velocity.Z;
+                    break;
+                default:
+                    throw new InvalidOperationException ();
                 }
                 if (rtn) { return false; }
             }
